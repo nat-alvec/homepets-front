e64c5d2a4d54c743effcd0b455db2bbddf320287
Natália Alves <natalia.alvescam@gmail.com>
2021-10-02 13:59:49 -0300

    adiciona a rota de user-details

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
+//Importing Libraries
+
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
+//Importing components
+
 import Home from "../routeComponents/Home";
 import AuthRouter from "../routeComponents/auth/AuthRouter";
-
 import { AuthContextComponent } from "../contexts/authContext";
+import UserDetails from "../components/User-details/User-details"
+
+//App Function
 
 function App() {
   return (
@@ -13,6 +20,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/auth" component={AuthRouter} />
+          <Route path="/detalhes-cuidador" component={UserDetails} />
         </Switch>
       </AuthContextComponent>
     </BrowserRouter>
